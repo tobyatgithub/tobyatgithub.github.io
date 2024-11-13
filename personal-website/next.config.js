4380d0649089e4562e1cd9b119729ce73d890e53
tobyatgithub <toby.fangyuan@gmail.com>
2024-11-13 00:27:26 -0800

    limit webpack chunk size to 20mb.

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Only apply to edge runtime
+    if (isServer && process.env.NEXT_RUNTIME === 'edge') {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxSize: 20 * 1024 * 1024, // 20MB to be safe
+        cacheGroups: {
+          default: false,
+          vendors: false,
+        },
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig 
